6bbf6392200ae974d76dcbca6b2f6d95152fc0fc
Bernhard Bablok <bablokb@gmx.de>
2023-06-21 16:43:31 +0200

    global object for config-variables

--- src/main.py
+++ src/main.py
@@ -54,17 +54,19 @@ except:
 # --- default configuration is in config.py on the pico.
 #     You can override it also with a config.py on the sd-card   -------------
 
-def import_config():
-  """ import config-module and make variables global """
-  import config
-  for var in dir(config):
-    if var[0] != '_':
-      g_logger.print(f"{var}={getattr(config,var)}")
-      globals()[var] = getattr(config,var)
-  config = None
-  gc.collect()
+class Settings:
+  def import_config(self):
+    """ import config-module and make variables attributes """
+    import config
+    for var in dir(config):
+      if var[0] != '_':
+        g_logger.print(f"{var}={getattr(config,var)}")
+        setattr(self,var) = getattr(config,var)
+    config = None
+    gc.collect()
 
-import_config()
+g_config = Settings()
+g_config.import_config()
 
 # --- pin-constants (don't change unless you know what you are doing)   ------
 
@@ -98,12 +100,9 @@ class DataCollector():
   def setup(self):
     """ create hardware-objects """
 
-    # spi - SD-card and display
-    if HAVE_SD:
-      self._spi = busio.SPI(PIN_SD_SCK,PIN_SD_MOSI,PIN_SD_MISO)
-
     # early setup of SD-card (in case we send debug-logs to sd-card)
-    if HAVE_SD:
+    if g_config.HAVE_SD:
+      self._spi = busio.SPI(PIN_SD_SCK,PIN_SD_MOSI,PIN_SD_MISO)
       self.sd_cs = DigitalInOut(PIN_SD_CS)
       sdcard     = adafruit_sdcard.SDCard(self._spi,self.sd_cs)
       self.vfs   = storage.VfsFat(sdcard)
@@ -111,7 +110,7 @@ class DataCollector():
       try:
         import sys
         sys.path.insert(0,"/sd")
-        import_config()
+        g_config.import_config()
         sys.path.pop(0)
       except:
         g_logger.print("no configuration found in /sd/config.py")
@@ -120,10 +119,11 @@ class DataCollector():
     i2c = busio.I2C(PIN_SCL,PIN_SDA)
 
     # If our custom PCB is connected, we have an RTC. Initialise it.
-    if HAVE_PCB:
-      self.rtc = ExtRTC(i2c,net_update=NET_UPDATE)  # this will also clear interrupts
-      self.rtc.rtc_ext.high_capacitance = True      # the pcb uses a 12.5pF capacitor
-      self.rtc.update()                             # (time-server->)ext-rtc->int-rtc
+    if g_config.HAVE_PCB:
+      self.rtc = ExtRTC(i2c,
+        net_update=g_config.NET_UPDATE)         # this will also clear interrupts
+      self.rtc.rtc_ext.high_capacitance = True  # the pcb uses a 12.5pF capacitor
+      self.rtc.update()                         # (time-server->)ext-rtc->int-rtc
 
     self.done           = DigitalInOut(PIN_DONE)
     self.done.direction = Direction.OUTPUT
@@ -133,23 +133,22 @@ class DataCollector():
     self.vbus_sense.direction = Direction.INPUT
 
     # display
-    global HAVE_DISPLAY
-    if HAVE_DISPLAY:
+    if g_config.HAVE_DISPLAY:
 
       displayio.release_displays()
 
       # spi - if not already created
-      if not HAVE_SD:
+      if not g_config.HAVE_SD:
         self._spi = busio.SPI(PIN_SD_SCK,PIN_SD_MOSI)
 
-      if HAVE_DISPLAY == "Inky-Pack":
+      if g_config.HAVE_DISPLAY == "Inky-Pack":
         self.display = DisplayFactory.inky_pack(self._spi)
-      elif HAVE_DISPLAY == "Display-Pack":
+      elif g_config.HAVE_DISPLAY == "Display-Pack":
         self.display = DisplayFactory.display_pack(self._spi)
         self.display.auto_refresh = False
       else:
-        g_logger.print(f"unsupported display: {HAVE_DISPLAY}")
-        HAVE_DISPLAY = None
+        g_logger.print(f"unsupported display: {g_config.HAVE_DISPLAY}")
+        g_config.HAVE_DISPLAY = None
       self._view = None
 
     # sensors
@@ -212,7 +211,7 @@ class DataCollector():
       self.csv_header += ',Noise'
 
     # just for testing
-    if TEST_MODE:
+    if g_config.TEST_MODE:
       self._led            = DigitalInOut(board.LED)
       self._led.direction  = Direction.OUTPUT
 
@@ -244,7 +243,7 @@ class DataCollector():
       width=self.display.width-2*border-(dim[1]-1)*divider,
       height=int(0.6*self.display.height),
       justify=Justify.LEFT,
-      fontname=f"fonts/{FONT_DISPLAY}.bdf",
+      fontname=f"fonts/{g_config.FONT_DISPLAY}.bdf",
       formats=self._formats,
       border=border,
       divider=divider,
@@ -257,12 +256,12 @@ class DataCollector():
       self._view.justify(Justify.RIGHT,index=i+1)
 
     # create DataPanel
-    title = PanelText(text=f"{LOGGER_TITLE}",
-                      fontname=f"fonts/{FONT_DISPLAY}.bdf",
+    title = PanelText(text=f"{g_config.LOGGER_TITLE}",
+                      fontname=f"fonts/{g_config.FONT_DISPLAY}.bdf",
                       justify=Justify.CENTER)
 
     self._footer = PanelText(text=f"Updated: ",
-                             fontname=f"fonts/{FONT_DISPLAY}.bdf",
+                             fontname=f"fonts/{g_config.FONT_DISPLAY}.bdf",
                              justify=Justify.RIGHT)
     self._panel = DataPanel(
       width=self.display.width,
@@ -291,8 +290,8 @@ class DataCollector():
   def continuous_mode(self):
     """ returns false if on USB-power """
 
-    return FORCE_CONT_MODE or (
-            self.vbus_sense.value and not FORCE_STROBE_MODE)
+    return g_config.FORCE_CONT_MODE or (
+            self.vbus_sense.value and not g_config.FORCE_STROBE_MODE)
 
   # --- collect data   -------------------------------------------------------
 
@@ -428,14 +427,14 @@ class DataCollector():
   def save_data(self):
     """ save data """
 
-    if SHOW_UNITS:
+    if g_config.SHOW_UNITS:
       self.pretty_print()
     else:
       g_logger.print(self.record)
 
-    if HAVE_SD:
+    if g_config.HAVE_SD:
       YMD = self.data["ts"].split("T")[0]
-      outfile = f"/sd/log_{LOGGER_ID}_{YMD}.csv"
+      outfile = f"/sd/log_{g_config.LOGGER_ID}_{YMD}.csv"
       new_csv = not self.file_exists(outfile)
       self.save_status = ":("
       with open(outfile, "a") as f:
@@ -487,8 +486,8 @@ class DataCollector():
 
   def configure_wakeup(self):
     """ configure rtc for next wakeup """
-    if HAVE_PCB:
-      self.rtc.set_alarm(self.rtc.get_alarm_time(m=OFF_MINUTES))
+    if g_config.HAVE_PCB:
+      self.rtc.set_alarm(self.rtc.get_alarm_time(m=g_config.OFF_MINUTES))
 
   # --- shutdown   -----------------------------------------------------------
 
@@ -510,7 +509,7 @@ class DataCollector():
 # --- main program   ---------------------------------------------------------
 
 g_logger.print("main program start")
-if TEST_MODE:
+if g_config.TEST_MODE:
   time.sleep(5)                        # give console some time to initialize
 g_logger.print("setup of hardware")
 
@@ -518,8 +517,8 @@ app = DataCollector()
 app.setup()
 
 while True:
-  if TEST_MODE:
-    app.blink(count=BLINK_START, blink_time=BLINK_TIME_START)
+  if g_config.TEST_MODE:
+    app.blink(count=g_config.BLINK_START, blink_time=g_config.BLINK_TIME_START)
 
   app.collect_data()
   try:
@@ -529,10 +528,10 @@ while True:
     app.cleanup()
     raise
     
-  if TEST_MODE:
-    app.blink(count=BLINK_END, blink_time=BLINK_TIME_END)
+  if g_config.TEST_MODE:
+    app.blink(count=g_config.BLINK_END, blink_time=g_config.BLINK_TIME_END)
 
-  if HAVE_DISPLAY:
+  if g_config.HAVE_DISPLAY:
     try:
       app.update_display()
     except:
@@ -540,13 +539,13 @@ while True:
       app.cleanup()
       raise
 
-  if HAVE_LORA:
+  if g_config.HAVE_LORA:
     app.send_data()
 
   # check if running on USB and sleep instead of shutdown
   if app.continuous_mode():
-    g_logger.print(f"continuous mode: next measurement in {CONT_INT} seconds")
-    time.sleep(CONT_INT)
+    g_logger.print(f"continuous mode: next measurement in {g_config.CONT_INT} seconds")
+    time.sleep(g_config.CONT_INT)
   else:
     break
 
