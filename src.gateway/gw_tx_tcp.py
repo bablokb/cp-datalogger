30e899c5e69eadeb7dd5fa126a7a88ef07888049
Bernhard Bablok <bablokb@gmx.de>
2026-02-02 12:10:54 +0100

    reimplement sending buffered data

--- src.gateway/gw_tx_tcp.py
+++ src.gateway/gw_tx_tcp.py
@@ -9,7 +9,8 @@
 import os
 import time
 
-CHUNK = 1024   # chunk-size for TCP transmission
+BUFFER_FILE     = "/sd/tx_buffer.csv"
+BUFFER_FILE_NEW = "/sd/tx_buffer_new.csv"
 
 # --- early configuration of the log-destination   ---------------------------
 
@@ -49,12 +50,12 @@ class TCPSender:
     if not hasattr(self._config,"HAVE_SD"):  # no SD, no buffered daa
       return
 
-    buffer_file = "/sd/tx_buffer.csv"
+    # check if buffer file exists
     try:
-      status = os.stat(buffer_file)
+      status = os.stat(BUFFER_FILE)
       size = status[6]
       if size == 0:
-        g_logger.print(f"TCPSender: empty file /sd/tx_buffer.csv")
+        g_logger.print(f"TCPSender: empty file {BUFFER_FILE}")
         os.remove(buffer_file)
         os.sync()
         return
@@ -62,32 +63,70 @@ class TCPSender:
         g_logger.print(f"TCPSender: size of buffered data: {size}")
     except:
       # file does not exist
-      g_logger.print(f"TCPSender: no data file /sd/tx_buffer.csv")
+      g_logger.print(f"TCPSender: no data file {BUFFER_FILE}")
       return
 
-    # fetch all data and send it. Efficient, but not robust.
+    # send buffer file in line mode
     host = self._config.TCP_HOST
     port = self._config.TCP_PORT
     g_logger.print(f"TCPSender: sending buffered data to {host}:{port}...")
-    sent = 0
     socket = None
-    with open(buffer_file,"rb") as file:
-      data = file.read(CHUNK)
-      g_logger.print(f"TCPSender: sending {len(data)} bytes")
-      socket, n = self._wifi.send(data,host,port,socket)
-      sent += n
-    socket.close()
-    g_logger.print(f"TCPSender: total bytes sent: {sent}")
-    if sent == size:
-      g_logger.print(f"TCPSender: deleting {buffer_file}")
-      os.remove(buffer_file)
+    buffer_file_new = None
+    rc_all = True
+
+    i = 0
+    with open(BUFFER_FILE,"rt") as file:
+      for record in file:
+        if not rc_all:
+          # we failed already, # so move records to BUFFER_FILE_NEW
+          if not buffer_file_new:
+            buffer_file_new = open(BUFFER_FILE_NEW,"at")
+          buffer_file_new.write(record)
+          continue
+
+        # convert values to bytes and send them
+        g_logger.print(f"TCPSender: sending: {record} ...")
+        try:
+          socket, n = self._wifi.send(
+            bytes(record,"UTF-8"),
+            self._config.TCP_HOST,self._config.TCP_PORT,
+            socket=socket)
+          g_logger.print(f"TCPSender: ... sent {n} bytes")
+          rc = n == len(record)
+        except Exception as ex:
+          g_logger.print(f"TCPSender: ... failed with exception: {ex}")
+          rc = False
+
+        # check result
+        rc_all = rc and rc_all
+        if not rc:
+          if i == 0:
+            # failed at the first record, bail out
+            return False
+          elif not buffer_file_new:
+            buffer_file_new = open(BUFFER_FILE_NEW,"at")
+          # keep this record in buffer_file_new
+          buffer_file_new.write(record)
+        i += 1
+
+    # at this stage, BUFFER_FILE is processed
+    if socket:
+      socket.close()
+    os.remove(BUFFER_FILE)
+    if buffer_file_new:
+      # move failed records to BUFFER_FILE, will be processed next time
+      buffer_file_new.flush()
+      buffer_file_new.close()
+      os.rename(BUFFER_FILE_NEW,BUFFER_FILE)
       os.sync()
-    return
+
+    # return send-status
+    return rc_all
 
   # --- process data   -------------------------------------------------------
 
   def process_data(self, msg_type, values):
-    """ process data  """
+    """ process data, single record  """
 
     g_logger.print("TCPSender: processing sensor-data...")
     start = time.monotonic()
