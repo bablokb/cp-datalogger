d80f9b54ed8ece73be36fd13ee1582403aaf48e3
Bernhard Bablok <bablokb@gmx.de>
2026-04-03 16:18:17 +0200

    refactor Influx-DB processing to datalogger_shared

--- src.receiver_service/files/usr/local/bin/datalogger_influxloader.py
+++ src.receiver_service/files/usr/local/bin/datalogger_influxloader.py
@@ -12,33 +12,9 @@
 #-----------------------------------------------------------------------------
 
 import argparse
-import json
 import sys
-import time
-from datetime import datetime as dt
 
-import requests
-
-from datalogger_shared import Tools
-import sensor_meta
-
-# --- helper class for InfluxDB parameters   ---------------------------------
-
-class IfxDB:
-  def __init__(self,tools):
-    """ constructor """
-    self.url = tools.get_value("INFLUXDB","url",
-                               "http://localhost:8086")
-    self.api = tools.get_value("INFLUXDB","api",
-                               "api/v2/write")
-    self.org = tools.get_value("INFLUXDB","org",
-                               "def_org")
-    self.bucket = tools.get_value("INFLUXDB","bucket",
-                                  "def_bucket")
-    self.token = tools.get_value("INFLUXDB","token",
-                                  "whatever")
-    self.endpoint = (f'{self.url}/{self.api}?org={self.org}&' +
-                     f'bucket={self.bucket}&precision=s')
+from datalogger_shared import Tools, IfxDB
 
 # --- DataLoader class   -----------------------------------------------------
 
@@ -48,75 +24,20 @@ class DataLoader:
   def __init__(self,args):
     """ constructor """
     self._tools = Tools(debug=args.debug)
-    self._ifxdb = None
-
-  # --- read input and convert to line protocol   ----------------------------
-
-  def convert_input(self, infile):
-    """ process infile """
-
-    for line in map(str.rstrip, infile):
-      try:
-        # try csv first
-        measurement = sensor_meta.split_csv(line)
-      except:
-        # try json (output from datalogger_parser.py)
-        try:
-          measurement = json.loads(line)
-          if not isinstance(measurement,dict):
-            raise ValueError()
-        except:
-          self._tools.debug(f"skipping corrupt record: {line}")
-          continue
-      # every measurement contains 1..n values (individual sensor-outputs)
-      self._tools.debug(f"{measurement=}")
-      for values in measurement["record"]:
-        ifx_lp = f'{values["sensor"]},id={measurement["id"]} '
-        for i, field in enumerate(values["fields"]):
-          ifx_lp += f'{field[0]}={values["data"][i]},'
-        ifx_lp = ifx_lp.rstrip(',')
-        ifx_lp += f' {int(dt.fromisoformat(measurement["ts"]).timestamp())}\n'
-        yield bytes(ifx_lp,"utf-8")
-
-  # --- post data to InfluxDB   ----------------------------------------------
-
-  def post_data(self, infile):
-    """ post data to InfluxDB
-
-    infile: a generator
-    outfile: an open file
-    """
-
-    headers = {
-      'Authorization': f'Token {self._ifxdb.token}',
-      'Accept': 'application/json',
-      "Content-Type":"application/octet-stream",
-      }
-
-    try:
-      start = time.monotonic()
-      response = requests.post(self._ifxdb.endpoint,
-                               headers=headers,
-                               data=infile
-                               )
-      duration = time.monotonic()-start
-      print(f"HTTP-Code: {response.status_code}\nText: {response.text}")
-      print(f"duration: {duration}s")
-    except Exception as ex:
-      print(f"HTTP-POST failed with exception: {ex}")
 
   # --- main loop   ----------------------------------------------------------
 
   def run(self, infile, outfile):
     """ process all data """
+    ifxdb = IfxDB(self._tools)
     if outfile is None:
       # read InfluxDB configuration/credentials
-      self._ifxdb = IfxDB(self._tools)
-      self.post_data(self.convert_input(infile))
+      code,text,duration = ifxdb.post_data(infile)
+      print(f"HTTP-Code: {code}\nText: {text}")
+      print(f"duration: {duration}s")
     else:
-      for data in self.convert_input(infile):
+      for data in ifxdb.convert_input(infile,encode=False):
         outfile.write(data)
-    if outfile:
       outfile.flush()
 
 # --- main program   ---------------------------------------------------------
--- src.receiver_service/files/usr/local/bin/datalogger_shared.py
+++ src.receiver_service/files/usr/local/bin/datalogger_shared.py
@@ -7,8 +7,17 @@
 # Website: https://github.com/bablokb/cp-datalogger
 #-----------------------------------------------------------------------------
 
-import sys
 import configparser
+from datetime import datetime as dt
+import json
+import sys
+import time
+
+import requests
+
+import sensor_meta
+
+# --- helper class for generic methods   -------------------------------------
 
 class Tools:
   def __init__(self,config_file='/etc/datalogger.conf',debug=False):
@@ -44,3 +53,72 @@ class Tools:
     else:
       value = default
     return value
+
+# --- helper class for InfluxDB parameters   ---------------------------------
+
+class IfxDB:
+  def __init__(self,tools):
+    """ constructor """
+    self._tools = tools
+    self.url = tools.get_value("INFLUXDB","url",
+                               "http://localhost:8086")
+    self.api = tools.get_value("INFLUXDB","api",
+                               "api/v2/write")
+    self.org = tools.get_value("INFLUXDB","org",
+                               "def_org")
+    self.bucket = tools.get_value("INFLUXDB","bucket",
+                                  "def_bucket")
+    self.token = tools.get_value("INFLUXDB","token",
+                                  "whatever")
+    self.endpoint = (f'{self.url}/{self.api}?org={self.org}&' +
+                     f'bucket={self.bucket}&precision=s')
+
+  # --- read input and convert to line protocol   ----------------------------
+
+  def convert_input(self, infile, encode=True):
+    """ process infile """
+
+    for line in map(str.rstrip, infile):
+      try:
+        # try csv first
+        measurement = sensor_meta.split_csv(line)
+      except:
+        # try json (output from datalogger_parser.py)
+        try:
+          measurement = json.loads(line)
+          if not isinstance(measurement,dict):
+            raise ValueError()
+        except:
+          self._tools.debug(f"skipping corrupt record: {line}", force=True)
+          continue
+      # every measurement contains 1..n values (individual sensor-outputs)
+      self._tools.debug(f"{measurement=}")
+      for values in measurement["record"]:
+        ifx_lp = f'{values["sensor"]},id={measurement["id"]} '
+        for i, field in enumerate(values["fields"]):
+          ifx_lp += f'{field[0]}={values["data"][i]},'
+        ifx_lp = ifx_lp.rstrip(',')
+        ifx_lp += f' {int(dt.fromisoformat(measurement["ts"]).timestamp())}\n'
+        yield bytes(ifx_lp,"utf-8") if encode else ifx_lp
+
+  # --- post data to InfluxDB   ----------------------------------------------
+
+  def post_data(self, infile):
+    """ post data to InfluxDB
+
+    infile: open file or list of strings
+    """
+
+    headers = {
+      'Authorization': f'Token {self.token}',
+      'Accept': 'application/json',
+      "Content-Type":"application/octet-stream",
+      }
+
+    start = time.monotonic()
+    response = requests.post(self.endpoint,
+                             headers=headers,
+                             data=self.convert_input(infile)
+                             )
+    duration = time.monotonic()-start
+    return (response.status_code,response.text,duration)
